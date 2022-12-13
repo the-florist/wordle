d6dcd50220a14fcce3432a1c64edb4cf768fa988
Ericka Florio <eaf49@cam.ac.uk>
2022-12-13 11:50:11 +0000

    Made all starting files.

---
+++ wordle.cpp
@@ -0,0 +1,11 @@
+//A c++ program which solves the Wordle independently and interactively.
+//Created December 2022 by Ericka Florio
+//For the purpose of assessment in the Michaelmas 2022 Research in Computing course.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main() {
+
+}

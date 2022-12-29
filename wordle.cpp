a2df16880e0d1e73f0f3b3cd8031aec49ae3c684
Ericka Florio <eaf49@cam.ac.uk>
2022-12-29 12:16:10 -0800

    wordle header alteration

--- wordle.cpp
+++ wordle.cpp
@@ -1,6 +1,8 @@
-//A c++ program which solves the Wordle independently and interactively.
-//Created December 2022 by Ericka Florio
-//For the purpose of assessment in the Michaelmas 2022 Research in Computing course.
+/*********
+A c++ program which solves the Wordle independently and interactively.
+Created December 2022 by Ericka Florio
+For the purpose of assessment in the Michaelmas 2022 Research in Computing course.
+**********/
 
 //#include "header.hpp"
 #include "modes.cpp"
